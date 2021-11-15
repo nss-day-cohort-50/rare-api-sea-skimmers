e26e644a1ffa0d81c474bf65ebf4fd3e35b3ac6e
trey shanks <shanksleslietrey@gmail.com>
2021-11-15 13:44:26 -0600

    register not working

--- rare/urls.py
+++ rare/urls.py
@@ -18,11 +18,14 @@ from django.conf.urls import include
 from rest_framework import routers
 from django.urls import path
 
+from rareapi.views import register_user
+
 router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('register', register_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 
 ]
