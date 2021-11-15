8d7f6f8ad43db50ca22e0e776fba259c14fa1fba
Alexander Schroyer <alexanderschroyer@gmail.com>
2021-11-15 11:35:19 -0600

    models migrated

--- rareapi/models/subscription.py
+++ rareapi/models/subscription.py
@@ -3,6 +3,6 @@ from django.db import models
 class Subscription(models.Model):
     """Subscription model"""
     created_on = models.DateField()
-    follower = models.ForeignKey("Author", on_delete=models.CASCADE)
+    follower = models.ManyToManyField("Author", related_name="following")
     author = models.ForeignKey("Author", on_delete=models.CASCADE)
     ended_on = models.DateField()
