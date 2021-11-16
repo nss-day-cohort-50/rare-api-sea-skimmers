72c9f1b0f95231d042ead48218f4fa8e82547743
trey shanks <shanksleslietrey@gmail.com>
2021-11-16 15:41:42 -0600

    login working

--- rare/urls.py
+++ rare/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from rest_framework import routers
 from rareapi.models import Author
 
-from rareapi.views import register_user, PostView, CategoryView
+from rareapi.views import register_user, login_user, PostView, CategoryView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'categories', CategoryView, 'category')
@@ -29,5 +29,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('register', register_user),
+    path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
